efc38b17f8ca7268c451e2a1bdb6a8e577cc5369
Dan Lester <dan@danlester.com>
2020-02-07 16:03:46 +0000

    Fix for GroupedInnotation - would not always instantiate children correctly

--- jupyter-innotater/jupyter_innotater/combine.py
+++ jupyter-innotater/jupyter_innotater/combine.py
@@ -10,7 +10,7 @@ class GroupedInnotation(Innotation):
 
     def __init__(self, *args, **kwargs):
 
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
 
         self.childinnotations = args
 
